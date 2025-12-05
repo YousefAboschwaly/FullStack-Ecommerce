1901b8ce7bba5e305a6e0dd31dfffffd8a67521a
yousef <yousefshwaly@gmail.com>
2025-12-05 21:44:46 +0200

    Inatilize the TanstackQuery library

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
 import About from "./pages/About";
+import Home from "./pages/Home";
 import Products from "./pages/Products";
-
 export default function App() {
+
   return (
-    <>
       <Routes>
         <Route index element={<Home/>}/>
         <Route path="about" element={<About/>}/>
         <Route path="products" element={<Products/>}/>
       </Routes>
-    </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "./components/ui/provider.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Provider>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Provider>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
