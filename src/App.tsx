c84fa51fbf12bdf6d339a7f918fc3b0eea31db52
yousef <yousefshwaly@gmail.com>
2025-12-05 11:47:12 +0200

    Install and Setup ChakraUi

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { Button, HStack } from "@chakra-ui/react";
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+ <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
+      
+    </>
   )
 }
