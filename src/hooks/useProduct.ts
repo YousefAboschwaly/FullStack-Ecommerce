17c7c5624274405074995b9106484ddf702cebae
yousef <yousefshwaly@gmail.com>
2025-12-07 11:58:54 +0200

    Create useProduct hook

---
+++ src/hooks/useProduct.ts
@@ -0,0 +1,15 @@
+import type { IProduct } from "@/interfaces";
+import { getProduct } from "@/services/apiProducts";
+import { useQuery } from "@tanstack/react-query";
+
+interface ProductsResponse {
+  data: IProduct;
+}
+export default function useProducts(id:string) {
+  const { data, isLoading, error } = useQuery<ProductsResponse>({
+    queryKey: ["product",id],
+    queryFn: ()=> getProduct(id),
+  });
+
+  return { data: data?.data ?? {}, isLoading, error };
+}
