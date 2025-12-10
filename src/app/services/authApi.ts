672dd9360ffb68e733a23968d062383fc843d5b7
yousef <yousefshwaly@gmail.com>
2025-12-10 04:55:26 +0200

    Create Login api

---
+++ src/app/services/authApi.ts
@@ -0,0 +1,20 @@
+import { fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi } from "@reduxjs/toolkit/query/react";
+
+export const authApi = createApi({
+  reducerPath:"authApi",
+  baseQuery : fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
+  endpoints:(build)=>({
+    login:build.mutation({
+      query(body) {
+        console.log(body)
+          return{
+            url:'/auth/local',
+            method: "POST",
+            
+          }
+      },
+    })
+  })
+  
+});
