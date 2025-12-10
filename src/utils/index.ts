47042d77bbfdca088a7d9450171ee2ac4829b603
yousef <yousefshwaly@gmail.com>
2025-12-10 23:03:43 +0200

    make SaveAuth function to setCookie

---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+import cookieService from "@/app/services/cookieService";
+
+export function saveAuth(data: string) {
+  const expires = new Date(Date.now() + 3 * 24 * 60 * 60 * 1000); // <- Date
+
+  const options = { expires, path: "/" };
+  cookieService.setCookie("jwt", data, options);
+}
