b2f01e466f368487dbacf806e725107bde93172e
yousef <yousefshwaly@gmail.com>
2025-12-17 16:16:22 +0200

    Create editProduct endPont

--- src/app/services/products.ts
+++ src/app/services/products.ts
@@ -10,45 +10,57 @@ export const productsApi = createApi({
   }),
   endpoints: (builder) => ({
     // GET All Products
-getProducts: builder.query<{ data: IProduct[] }, { page: number }>({
-  query: ({ page }) =>
-    `/api/products?fields=title,description,price,stock&populate=*&pagination[pageSize]=12&pagination[page]=${page}`,
-  providesTags: (result) =>
-    result
-      ? [
-          { type: "Products", id: "LIST" },
-          ...result.data.map((product) => ({
-            type: "Products" as const,
-            id: product.documentId,
-          })),
-        ]
-      : [{ type: "Products", id: "LIST" }],
-}),
+    getProducts: builder.query<{ data: IProduct[] }, { page: number }>({
+      query: ({ page }) =>
+        `/api/products?fields=title,description,price,stock&populate=*&pagination[pageSize]=12&pagination[page]=${page}`,
+      providesTags: (result) =>
+        result
+          ? [
+              { type: "Products", id: "LIST" },
+              ...result.data.map((product) => ({
+                type: "Products" as const,
+                id: product.documentId,
+              })),
+            ]
+          : [{ type: "Products", id: "LIST" }],
+    }),
 
-
-    // GET Product Details 
-    getProduct: builder.query<{data:IProduct}, string>({
+    // GET Product Details
+    getProduct: builder.query<{ data: IProduct }, string>({
       query: (id) =>
         `/api/products/${id}?populate=*&fields=title,description,price,stock`,
-      providesTags: (_result, _error, id) => [
+      providesTags: (_result, _error, id) => [{ type: "Products", id }],
+    }),
+
+    // EDIT Product
+
+    editAdminProduct: builder.mutation<undefined,{ id: string; body: ProductFormData }>({
+      query: ({ id, body }) => ({
+        url: `/api/products/${id}`,
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${cookieService.getCookie("jwt")}`,
+        },
+        body,
+      }),
+      invalidatesTags: (_result, _error, { id }) => [
         { type: "Products", id },
+        { type: "Products", id: "LIST" },
       ],
     }),
-    
 
-    // DELETE Product 
+    // DELETE Product
     deleteAdminProduct: builder.mutation<undefined, string>({
-      query: (id) =>{
-        return{
-          url:`/api/products/${id}`,
-          method:"DELETE",
-          headers:{
-            Authorization:`Bearer ${cookieService.getCookie("jwt")}`
-          }
-        }
+      query: (id) => {
+        return {
+          url: `/api/products/${id}`,
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${cookieService.getCookie("jwt")}`,
+          },
+        };
       },
-       invalidatesTags:["Products"]
-
+      invalidatesTags: ["Products"],
     }),
   }),
 });
@@ -56,5 +68,5 @@ getProducts: builder.query<{ data: IProduct[] }, { page: number }>({
 export const {
   useGetProductsQuery,
   useGetProductQuery,
-  useDeleteAdminProductMutation
+  useDeleteAdminProductMutation,
 } = productsApi;
